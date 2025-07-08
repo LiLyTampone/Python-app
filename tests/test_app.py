ca16bd09d65f88080bbf9cc683ab50152767e361
LiLyTampone <nguyenminh569954@gmail.com>
2025-07-08 07:26:06 +0700

    Sua loi Unicode trong test

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert "Chào mừng" in response.data
+    assert "Chào mừng" in response.data.decode('utf-8')
